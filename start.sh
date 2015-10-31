b074330e3abdc3604a8d18ad71716bd389959e38
hieutrtr <hieutrantrung.it@gmail.com>
2015-10-31 21:30:37 +0700

    scp -r yum repos folder

--- start.sh
+++ start.sh
@@ -2,5 +2,5 @@ user=<your-user-on-server>
 host=<IP>
 
 ssh ${user}@${host} 'sudo rpm -qa --qf "%{NAME}-%{VERSION}-%{RELEASE}.%{ARCH}\n"' >> yum_list
-scp ${user}@${host}:/etc/yum.repos.d ./
+scp -r ${user}@${host}:/etc/yum.repos.d ./
 docker build -t hieutrtr/dockerize_all .
