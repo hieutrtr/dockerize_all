0e486daba82baa6dd6241c04a472910023b75c84
hieutrtr <hieutrantrung.it@gmail.com>
2015-10-31 17:38:49 +0700

    need to be config

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
-user="trunghieu"
-host="10.60.7.116"
+user=<your-user-on-server>
+host=<IP>
 
 ssh ${user}@${host} 'sudo rpm -qa --qf "%{NAME}-%{VERSION}-%{RELEASE}.%{ARCH}\n"' >> yum_list
 scp ${user}@${host}:/etc/yum.repos.d ./
