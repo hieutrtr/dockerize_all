e60464c9888c0554f739771bce0f3acb8c5019d4
hieutrtr <hieutrantrung@chotot.vn>
2015-10-31 17:31:34 +0700

    clone repo

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,7 @@ FROM centos:6.7
 
 RUN yum -y update
 ADD yum_list /yum_list
+ADD yum.repos.d /etc/yum.repos.d
 RUN yum -y install $(cat /yum_list)
 
 ENTRYPOINT []
--- start.sh
+++ start.sh
@@ -2,5 +2,5 @@ user="trunghieu"
 host="10.60.7.116"
 
 ssh ${user}@${host} 'sudo rpm -qa --qf "%{NAME}-%{VERSION}-%{RELEASE}.%{ARCH}\n"' >> yum_list
-
+scp ${user}@${host}:/etc/yum.repos.d ./
 docker build -t hieutrtr/dockerize_all .
