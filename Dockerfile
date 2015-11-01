61c7595988859dba84e6107f912fd590b40cc08e
hieutrtr <hieutrantrung.it@gmail.com>
2015-11-02 01:44:06 +0700

    clone rpm-gpg

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,7 @@ FROM centos:6.7
 RUN yum -y update
 ADD yum_list /yum_list
 ADD yum.repos.d /etc/yum.repos.d
+ADD rpm-gpg /etc/pki/rpm-gpg
 RUN yum -y install $(cat /yum_list)
 
 ENTRYPOINT []
--- start.sh
+++ start.sh
@@ -3,4 +3,5 @@ host=<IP>
 
 ssh ${user}@${host} 'sudo rpm -qa --qf "%{NAME}-%{VERSION}-%{RELEASE}.%{ARCH}\n"' >> yum_list
 scp -r ${user}@${host}:/etc/yum.repos.d ./
+scp -r ${user}@${host}:/etc/pki/rpm-gpg ./
 docker build -t hieutrtr/dockerize_all .
